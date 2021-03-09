5d6ad15b2a9634615e75db5ba158060d77322799
Tareq Hassan <tareqhassan2014@gmail.com>
2021-03-09 11:26:58 +0600

    nothing

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,7 +4,7 @@ import Movies from '../Movies/Movies';
 const Home = () => {
     return (
         <div>
-            <h1>This is Home</h1>
+            <h1>This is Home.</h1>
             <Movies/>
         </div>
     );
